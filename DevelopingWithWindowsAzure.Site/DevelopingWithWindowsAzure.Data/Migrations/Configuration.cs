774d7bc1f0f4428267b6f0ce3b0e6806ea4e4698
James Churchill <james@smashdev.com>
2012-10-22 10:56:38 -0700

    Updated references to copy to local

--- DevelopingWithWindowsAzure.Site/DevelopingWithWindowsAzure.Data/Migrations/Configuration.cs
+++ DevelopingWithWindowsAzure.Site/DevelopingWithWindowsAzure.Data/Migrations/Configuration.cs
@@ -6,27 +6,28 @@ namespace DevelopingWithWindowsAzure.Shared.Migrations
 	using System.Data.Entity.Migrations;
 	using System.Linq;
 
-    public sealed class Configuration : DbMigrationsConfiguration<Context>
-    {
-        public Configuration()
-        {
-            AutomaticMigrationsEnabled = false;
-        }
+	public sealed class Configuration : DbMigrationsConfiguration<Context>
+	{
+		public Configuration()
+		{
+			// JCTODO had to change this to true because I mistakenly changed the name of a migration class
+			AutomaticMigrationsEnabled = true;
+		}
 
-        protected override void Seed(Context context)
-        {
-            //  This method will be called after migrating to the latest version.
+		protected override void Seed(Context context)
+		{
+			//  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
-        }
-    }
+			//  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+			//  to avoid creating duplicate seed data. E.g.
+			//
+			//    context.People.AddOrUpdate(
+			//      p => p.FullName,
+			//      new Person { FullName = "Andrew Peters" },
+			//      new Person { FullName = "Brice Lambson" },
+			//      new Person { FullName = "Rowan Miller" }
+			//    );
+			//
+		}
+	}
 }
